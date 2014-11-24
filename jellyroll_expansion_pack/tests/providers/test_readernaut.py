9d58f6ed56e14893ecd2754c42809a1621e0ac60
Benjamin Turner <benturn@gmail.com>
2014-11-23 22:59:09 -0800

    update readernaut and test data

--- jellyroll_expansion_pack/tests/providers/test_readernaut.py
+++ jellyroll_expansion_pack/tests/providers/test_readernaut.py
@@ -5,40 +5,44 @@ import os
 
 from django.test import TestCase
 
-from jellyroll_expansion_pack.models import Book
+from jellyroll_expansion_pack.models import Book, BookProgress
 from jellyroll_expansion_pack.providers import readernaut
 
-json_file = open(os.path.join(
+f = open(os.path.join(
     os.path.dirname(__file__), 'fixtures/readernaut/response_1.json'))
+json_file = f.read()
+f.close()
 
 mock_getjson = mock.Mock()
-mock_getjson.return_value = json.load(json_file)
+mock_getjson.return_value = json.loads(json_file)
 
 
 class ReadernautProviderTestCase(TestCase):
-    @mock.patch('jellyroll.providers.utils.getjson', mock_getjson)
+    @mock.patch('jellyroll_expansion_pack.providers.readernaut.utils.getjson', mock_getjson)
     def setUp(self):
         readernaut.update()
 
     def test_enabled(self):
         self.assertEqual(readernaut.enabled(), True)
 
-    @mock.patch('jellyroll.providers.utils.getjson', mock_getjson)
+    @mock.patch('jellyroll_expansion_pack.providers.readernaut.utils.getjson', mock_getjson)
     def test_book_count_after_second_update(self):
-        self.assertEqual(Book.objects.all().count(), 5)
+        self.assertEqual(Book.objects.count(), 5)
+        self.assertEqual(BookProgress.objects.count(), 10)
 
-        readernaut.update()
-        self.assertEqual(Book.objects.all().count(), 5)
-
-    def test_multiple_authors(self):
         book = Book.objects.get(title='Teenage Mutant Ninja Turtles')
         self.assertEqual(book.author, 'Kevin B. Eastman, Peter Laird')
 
+        readernaut.update()
+        self.assertEqual(Book.objects.count(), 5)
+
+    @mock.patch('jellyroll_expansion_pack.providers.readernaut.utils.getjson', mock_getjson)
     @mock.patch('jellyroll_expansion_pack.providers.readernaut.log')
     def test_skips_updates(self, mock_log):
         readernaut.update()
         self.assertTrue(mock_log.info.called)
 
+    @mock.patch('jellyroll_expansion_pack.providers.readernaut.utils.getjson', mock_getjson)
     @mock.patch('jellyroll_expansion_pack.providers.readernaut.log')
     def test_force_updates(self, mock_log):
         readernaut.update(force=True)
